72b7ca9edba96f817b8b4384d10e62973dac5fdb
conniean <conniean@princeton.edu>
2020-11-09 20:36:22 -0500

    flattened c code

--- mywcflat.c
+++ mywcflat.c
@@ -29,7 +29,7 @@ int main(void)
 {
   loop1:
    /* while ((iChar = getchar()) != EOF)*/
-   if((iChar = getChar()) == EOF) goto endloop1;
+   if((iChar = getchar()) == EOF) goto endloop1;
    lCharCount++;
 /*
    if (isspace(iChar))
@@ -46,23 +46,33 @@ int main(void)
          iInWord = TRUE;
    }
 */
-   if(!isspace(ichar)) goto else1;
+   if(!isspace(iChar)) goto else1;
    if(!iInWord) goto endif2;
    lWordCount++;
    iInWord = FALSE;
   endif2:
-   
+   goto endif1;
+  else1: 
+   if(iInWord) goto endif3;
+   iInWord = TRUE;
+  endif3:
   endif1:
-   if (iChar == '\n')
+
+   /* if (iChar == '\n')
       lLineCount++;
+   */
+   if(iChar != '\n') goto endif4;
+   lLineCount++;
+  endif4:
+   goto loop1;
   endloop1:
 
 /*   if (iInWord)
       lWordCount++;
 */
-   if(!iInWord) goto endif6;
+   if(!iInWord) goto endif5;
    lWordCount++;
-  endif6:
+  endif5:
    
    printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
    return 0;
